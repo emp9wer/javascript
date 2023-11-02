9a4bd73871bf3e019b2e10645dd099332c0d6348
emp9wer <kirillkandakov@gmail.com>
2023-11-01 20:43:52 -0400

    add operators

---
+++ 03_Operators.js
@@ -0,0 +1,12 @@
+console.log(4+4*(-1)-8/2);
+
+let x = 5;
+x+=5;
+console.log(x);
+
+//comparison: simmilar to java with some diff
+
+let one = 1;
+let one_again = 1;
+let one_string = '1';
+let two_string = '2';
