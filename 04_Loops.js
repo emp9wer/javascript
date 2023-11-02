d0701300ab381da5ca710c3a04bec04e31a82c20
emp9wer <kirillkandakov@gmail.com>
2023-11-02 15:23:25 -0400

    Arrays loops added

--- 04_Loops.js
+++ 04_Loops.js
@@ -1,14 +1,28 @@
-// loops same as in java for loop , while, do while
+console.log("loops same as in java for loop , while, do while---------------------");
 
-for(let i = 0; i<5; i++){
+for(let i = 0; i<3; i++){
     console.log(i);
 }
 //console.log(i); //error //              !!!!let -> local scope - var -> global scope!!!!
 
 console.log("var-------old type of declaring variables before ES6 -> use let!!!  do not use var!!!------problem with loop");
-for(var j = 0; j<5; j++){
+for(var j = 0; j<3; j++){
     console.log(j);
 }
 console.log(j); //no error - variable //global and function scope
 
+
+console.log("while loops----------");
+let n1 = 0;
+while (n1<3) {
+    console.log(n1++);
+}
+
+console.log("do while loops----------");
+
+let n2 = 0;
+do {
+    console.log(n2);
+    n2++
+} while (n2<3);
 
---
+++ 05_Arrays.js
@@ -0,0 +1,78 @@
+/**
+ * there is no Collection in js -> only array and map
+ * not fixed size
+ * 2 important brackets [] -> array  /  {} -> object
+ */
+
+let scores = new Array(); // creates empty array
+console.log(scores);//[]
+
+let emptyArray = [];// creates empty array
+console.log(emptyArray);//[]
+
+let nums = new Array(10); // creates empty array with size.
+console.log(nums);//[ <10 empty items> ]
+
+let myNums = new Array(1,2,3,4,5,8);
+console.log(myNums);//[ 1, 2, 3, 4, 5, 8 ]
+
+let myNums1 = [1,2,3,4,5,8];
+console.log(myNums1);//[ 1, 2, 3, 4, 5, 8 ]
+
+
+let colors1 = ['red','green','blue'];
+console.log(colors1);//[ 'red', 'green', 'blue' ]
+console.log(colors1[1]);//green
+
+console.log("add element to array!---------------");
+
+let colors = new Array('red', 'green', 'blue');
+
+colors.push("white") // push -> adds to the end!                 //push
+console.log(colors);//[ 'red', 'green', 'blue', 'white' ]
+
+colors.unshift("orange")// unshift -> adds element to beginning  // unshift
+console.log(colors);//[ 'orange', 'red', 'green', 'blue', 'white' ]
+
+
+console.log("----------------pop -> gets and removes");
+console.log(colors);//[ 'orange', 'red', 'green', 'blue', 'white' ]
+console.log(colors.pop()); //white 
+console.log(colors);//[ 'orange', 'red', 'green', 'blue' ]
+
+console.log("---------------size of array");
+console.log(colors.length); //4
+
+for(let i =0; i<colors.length; i++){
+    console.log(colors[i]); // orange // red // green // blue
+}
+
+console.log('--------loop with values of array  //forof shortcut');
+for (const iterator of colors) {
+    console.log(iterator);
+}
+
+console.log('--------//foreach (commonly used!)');
+//foreach (commonly used!)
+colors.forEach(element => {
+    console.log(element);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
